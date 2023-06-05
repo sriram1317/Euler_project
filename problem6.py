e8e28dab4206933545468f4895d0ce42670b68b5
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 15:41:42 +0530

    Problem 6

--- problem6.py
+++ problem6.py
@@ -8,7 +8,7 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square 
 # of the sum is 3025 − 385 = 2640.
 
-# Find the difference between the sum of the squares of the first n natural numbers and the square of the sum.
+# Find the difference between the sum of the squares of the first n natural numbers and the square of sum.
 
 num = int(input("Provide the number: "))
 
