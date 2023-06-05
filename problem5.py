7a765d3757f239d4ee4d74f5d5becb6aafb47a57
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 15:32:17 +0530

    Problem 54

---
+++ problem5.py
@@ -0,0 +1,22 @@
+# Problem 5: Smallest multiple
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+
+
+num = int(input("enter te number: "))
+maxLcm = 1
+
+def lcm(a, b):
+    return int((a * b) / gcd(a, b))
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+    
+for i in range(1,num+1):
+    maxLcm = lcm(maxLcm, i)
+
+print(maxLcm)
